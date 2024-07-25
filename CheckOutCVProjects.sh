5f9d5ca6dc42ce1e6b73773e7dba43633bf6276c
aditijahagirdar <aditijahagirdar121@gmail.com>
2024-07-25 23:09:14 +0530

    chg:dev:script to clone CV projects

---
+++ CheckOutCVProjects.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#PROJECT_BRAIN_TUMOR_DETECTION
+git clone https://github.com/Aditi-Jahagirdar/Deep-Learning-Brain-MRI-Segmentation-UNet.git
+
+#PROJECT_FACE_MESH
+git clone https://github.com/Aditi-Jahagirdar/ComputerVision-FaceMesh.git
+
+#PROJECT_HAND_TRACKING
+git clone https://github.com/Aditi-Jahagirdar/ComputerVision-handTracking.git
+
+#PROJECT_POSE_ESTIMATION
+git clone https://github.com/Aditi-Jahagirdar/ComputerVision-PoseEstimation.git
+
+#PROJECT_FACE_DETECTION
+git clone https://github.com/Aditi-Jahagirdar/ComputerVision-FaceDetection.git
